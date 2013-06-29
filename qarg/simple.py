937fd9fff44fcd4c9fb60c651ce195a7c1797d09
Brett Graham <brettgraham@gmail.com>
2013-06-29 14:24:08 -0400

    docs

--- qarg/simple.py
+++ qarg/simple.py
@@ -8,6 +8,31 @@ class ParseError(Exception):
 
 
 def parse(arguments=None):
+    """
+    Simple argument parser
+
+    Options
+    ------
+
+    arguments : arguments to parse
+        if None, sys.argv[1:] will be used
+
+
+    Returns
+    ------
+
+    args : list of strings
+        arguments provided before options
+            i.e. all arguments before the first '-something' argument
+
+    opts : dictionary
+        keys = parsed arguments (e.g. -k -> 'k', --key -> 'key')
+        values = values of arguments
+            True if present
+            string if a value is provided (e.g. 'k': 'foo' if '-k foo')
+            list of strings if multiple values are provided
+                (e.g. 'k': ['foo', 'bar'] if '-k foo bar')
+    """
     if arguments is None:
         arguments = sys.argv[1:]
     opts = {}
