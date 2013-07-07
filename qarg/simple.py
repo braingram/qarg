1bf7fcbf85a2fcc1b250a58f2628bf3f9fd3d086
Brett Graham <brettgraham@gmail.com>
2013-07-07 11:12:25 -0400

    adding unparse, fixing bug where multiple opt args were being skipped, adding test function

--- qarg/simple.py
+++ qarg/simple.py
@@ -7,6 +7,10 @@ class ParseError(Exception):
     pass
 
 
+class UnParseError(Exception):
+    pass
+
+
 def parse(arguments=None):
     """
     Simple argument parser
@@ -42,6 +46,8 @@ def parse(arguments=None):
         if len(a) == 0:
             continue
         if a[0] == '-':
+            if a == '-':  # maybe this should 'reset' the key?
+                raise ParseError("Invalid '-' argument: %s" % a)
             if (len(a) > 1) and (a[1] == '-'):
                 # long opt
                 k = a[2:]
@@ -56,7 +62,7 @@ def parse(arguments=None):
             if k is None:
                 args.append(a)
             else:
-                if 'k' in opts:
+                if k in opts:
                     if isinstance(opts[k], bool):
                         opts[k] = a
                     elif isinstance(opts[k], list):
@@ -66,3 +72,76 @@ def parse(arguments=None):
                 else:
                     opts[k] = a
     return args, opts
+
+
+def key_to_opt(k):
+    if len(k) == 1:
+        return '-%s' % k
+    return '--%s' % k
+
+
+def unparse(args, opts):
+    arguments = args[:]
+    for k in opts:
+        arguments.append(key_to_opt(k))
+        if opts[k] is True:
+            continue
+        elif isinstance(opts[k], str):
+            arguments.append(opts[k])
+            continue
+        elif isinstance(opts[k], (tuple, list)):
+            arguments.extend(opts[k])
+            continue
+        raise UnParseError(
+            "Invalid option type or value: %s: %s"
+            % (k, opts[k]))
+    return arguments
+
+
+def test():
+    assert key_to_opt('a') == '-a'
+    assert key_to_opt('ab') == '--ab'
+
+    args = 'a b c -x 1 -y -z 1 2 3 --foo --bar'.split()
+    a, o = parse(args)
+    assert len(a) == 3
+    assert 'a' in a
+    assert 'b' in a
+    assert 'c' in a
+    assert len(o) == 5
+    assert 'x' in o
+    assert 'y' in o
+    assert 'z' in o
+    assert 'foo' in o
+    assert 'bar' in o
+    assert o['x'] == '1'
+    assert o['y'] is True
+    assert o['z'] == ['1', '2', '3']
+    assert o['foo'] is True
+    assert o['bar'] is True
+
+    ua = unparse(a, o)
+    assert len(ua) == len(args)
+    for a in args:
+        assert a in ua
+    for u in ua:
+        assert u in args
+
+    # check exceptions
+    try:
+        parse(['-'])
+        assert False
+    except ParseError:
+        pass
+
+    try:
+        parse(['-aa'])
+        assert False
+    except ParseError:
+        pass
+
+    try:
+        unparse([], {'a': 1})
+        assert False
+    except UnParseError:
+        pass
